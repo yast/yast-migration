8db55a322a03cba829c6240cd2ee80a00be02916
Josef Reidinger <jreidinger@suse.cz>
2015-04-01 17:10:23 +0200

    add explaining comment

--- src/clients/migration_proposals.rb
+++ src/clients/migration_proposals.rb
@@ -4,5 +4,6 @@ require "yast"
 
 Yast.import "Wizard"
 
+# Proposal runner expect already opened wizard dialog
 Yast::Wizard.OpenNextBackDialog
 ::Installation::ProposalRunner.new(Migration::ProposalStore).run
