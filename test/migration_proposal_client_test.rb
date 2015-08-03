731b8e7957ebceca16c794380734daa90af1b2bf
Ladislav Slezák <lslezak@suse.cz>
2015-08-03 16:15:41 +0200

    the correct copyright holder is "SUSE LLC"
    Conflicts:
    	test/migration_proposal_client_test.rb

--- test/migration_proposal_client_test.rb
+++ test/migration_proposal_client_test.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 # ------------------------------------------------------------------------------
-# Copyright (c) 2015 SUSE LINUX Products GmbH, Nuernberg, Germany.
+# Copyright (c) 2015 SUSE LLC, All Rights Reserved.
 #
 #
 # This program is free software; you can redistribute it and/or modify it under
@@ -13,7 +13,7 @@
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with
-# this program; if not, contact Novell, Inc.
+# this program; if not, contact SUSE LLC.
 #
 # To contact Novell about this file by physical or electronic mail, you may find
 # current contact information at www.novell.com.
